528226872f84e31c98788744c9da290464d6cf00
Miroslav Bauer <mirekys@users.noreply.github.com>
2021-03-19 13:45:22 +0100

    add community id to routes

--- src/constants.js
+++ src/constants.js
@@ -3,3 +3,5 @@ export const DATASETS_DRAFT_COLLECTION_CODE = 'datasets/draft'
 
 export const ARTICLES_COLLECTION_CODE = 'articles'
 export const ARTICLES_DRAFT_COLLECTION_CODE = 'articles/draft'
+
+export const COMMUNITIES = ['cesnet']
